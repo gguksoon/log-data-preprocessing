99a7d62346760f8827cd88fc12018a168d2b9259
gguksoon <mingsoo0807@gmail.com>
2022-03-04 11:25:37 +0900

    init

---
+++ src/main/java/kr/letech/LogDataPreprocessingApplication.java
@@ -0,0 +1,13 @@
+package kr.letech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDataPreprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogDataPreprocessingApplication.class, args);
+	}
+
+}
